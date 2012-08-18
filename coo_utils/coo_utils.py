a0514b86f1947262d7f4e3db83f60bfaa7c3d933
David N. Mashburn <david.n.mashburn@gmail.com>
2012-08-18 00:00:42 -0500

    Added docs to coo_utils.py

--- coo_utils/coo_utils.py
+++ coo_utils/coo_utils.py
@@ -1,9 +1,32 @@
+'''coo_utils works with numpy as scipy.sparse (2D only) to store
+compressed multidimensional arrays both in memory and on disks. This can
+DRASTICALLY reduce the space required.
+
+In reality, what is stored is a list-of-lists containing sparse matrix
+nodes in the scipy.sparse.coo_matrix format. The storage format can
+either be normal or differenced (helpful for same-value blocks data).
+
+Some terms used in the code:
+
+CooHD: list (or deeply nested list(s)) of scipy.sparse.coo_matrix's
+       (HD means higher-dimensional)
+nnzs: An array with the same shape as the nested lists in the cooHD
+      (aka without the 2 coo_matrix dimensions) that gives the length
+      of each coo_matrix (nnz) at each node of the tree
+RCD: A sparse array represented as a flat row-column-data matrix
+    Multi-dimensional shape data is recoverable using the nnzs array
+
+The disk storage format uses the RCD fomat for maximum simplicity:
+
+*_rcd.npy    -  The rcd matrix stored in the .npy format (int32)
+*_nnzs.npy   -  The nnzs array store in .npy format (int)
+*_shape.txt  -  a simple text file with the full shape of the array in
+                comma-separated plain text
+'''
+
 import numpy as np
 import scipy.sparse
 
-# CooHD / RCD Save and load
-# CooHD: list (or deeply nested list(s)) of scipy.sparse.coo_matrix's ; HD means higher-dimensional
-# nnzs: array (same shape as cooHD) of that gives the length of each cooMatrix at each node of the tree
 def VerifyCooHDShape(cooHD,shape):
     if len(shape)>2:                      # I'm a tree
         if len(cooHD)!=shape[0]:
