273f0766f3f2f1d66618eb7227825a1c32cda34e
David N. Mashburn <david.n.mashburn@gmail.com>
2012-11-11 02:14:34 -0600

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='coo_utils',
-    version='0.1.1',
+    version='0.1.2',
     author='David N. Mashburn',
     author_email='david.n.mashburn@gmail.com',
     packages=['coo_utils'],
